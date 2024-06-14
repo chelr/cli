e3adb36bab4499b263a8dd37c0f3170e030873d4
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-06-14 19:21:52 +0530

    almost done with the entire format of chelr

--- cmd/generate.js
+++
@@ -1,8 +0,0 @@
-import { intro, outro } from "@clack/prompts";
-
-const Generate = () => {
-  intro("integrate chelr");
-  outro("you're all set.");
-};
-
-export default Generate;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import Generate from "./cmd/generate.js";
+import { intro, text, outro, select, multiselect } from "@clack/prompts";
 
 const program = new Command();
 
@@ -13,6 +13,26 @@ program.command("ping").action(() => console.log("pong"));
 program
   .command("generate")
   .description("generate chelr app")
-  .action(Generate());
+  .action(async () => {
+    intro("integrate chelr");
+
+    const project_name = await text({
+      message: "Project name: ",
+      placeholder: "awesome-chelr-project",
+    });
+
+    const payment_choices = await multiselect({
+      message: "Pick ur payment methods: ",
+      options: [
+        { value: "bitcoin", label: "Bitcoin (btc)", hint: "popular" },
+        { value: "ethereum", label: "Ethereum (eth)" },
+        { value: "solana", label: "Solana (sol)" },
+      ],
+    });
+
+    console.log(project_name, payment_choices);
+
+    outro("chelr integrated, have fun building the rest of your app.");
+  });
 
 program.parse();
